2c90e329ba9ab33960df27f102d27bb2f4fdacdf
PhantomCorner <jiaojiaobigbang@gmail.com>
2026-04-13 16:30:11 +0800

    navbar dropdown update

--- src/components/admin/Navbar.vue
+++ src/components/admin/Navbar.vue
@@ -1,3 +1,66 @@
+<script setup lang="ts">
+const userName = "Admin";
+const handleCommand = (command: string) => {
+  if (command === "logout") {
+    // Handle logout logic here
+    console.log("Logging out...");
+  }
+};
+</script>
+
 <template>
-  <div>Navbar</div>
+  <div class="nav-bar">
+    <div class="flex-box">
+      <el-button type="text">
+        <el-icon>
+          <Expand />
+        </el-icon>
+
+        <p class="page-title">Nav bar</p>
+      </el-button>
+    </div>
+    <div class="flex-box dropdown">
+      <el-dropdown placement="bottom" @command="handleCommand">
+        <div class="flex-box">
+          <el-avatar
+            src="https://cdn-icons-png.flaticon.com/512/4712/4712039.png"
+            alt="avatar"
+            size="default" />
+          <span style="margin-left: 8px">{{ userName }}</span>
+        </div>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <!-- <el-dropdown-item>Profile</el-dropdown-item>
+            <el-dropdown-item>Settings</el-dropdown-item> -->
+            <el-dropdown-item command="logout">Logout</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.nav-bar {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 64px;
+  background-color: #ecf0f1;
+  padding: 0 16px;
+  .page-title {
+    font-size: 24px;
+    margin-left: 8px;
+    color: #2c3e50;
+    font-weight: bold;
+  }
+  .flex-box {
+    display: flex;
+    align-items: center;
+    justify-self: center;
+  }
+  .dropdown {
+    cursor: pointer;
+  }
+}
+</style>
--- src/components/admin/Sidebar.vue
+++ src/components/admin/Sidebar.vue
@@ -1,10 +1,9 @@
 <template>
   <el-aside width="248px" class="sidebar">
     <el-menu
-      default-active="2"
-      class="el-menu-vertical-demo"
-      @open="handleOpen"
-      @close="handleClose">
+      :default-active="activeMenu"
+      :router="true"
+      class="el-menu-vertical-demo">
       <div class="brand">
         <el-image
           class="brand-icon"
@@ -16,10 +15,9 @@
         </div>
       </div>
       <el-menu-item
-        v-for="route in router.options.routes[0].children"
-        :index="route.path"
-        :key="route.path"
-        @click="router.push(route.path)">
+        v-for="route in menuRoutes"
+        :index="`${basePath}/${route.path}`"
+        :key="route.path">
         <el-icon>
           <component :is="route.meta?.icon" />
         </el-icon>
@@ -30,16 +28,16 @@
 </template>
 
 <script lang="ts" setup>
-import {useRouter} from "vue-router";
-const router = useRouter();
-// console.log(router);
+import {computed} from "vue";
+import {useRoute, useRouter} from "vue-router";
 
-const handleOpen = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath);
-};
-const handleClose = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath);
-};
+const router = useRouter();
+const route = useRoute();
+
+const adminRoute = router.options.routes[0];
+const basePath = adminRoute.path;
+const menuRoutes = adminRoute.children ?? [];
+const activeMenu = computed(() => route.path);
 </script>
 <style scoped lang="scss">
 .sidebar {
