92e87d058ca958ca2dda04c8993cb4bb02410bed
PhantomCorner <jiaojiaobigbang@gmail.com>
2026-04-13 16:10:25 +0800

    sidebar css update

--- src/components/admin/Sidebar.vue
+++ src/components/admin/Sidebar.vue
@@ -1,10 +1,20 @@
 <template>
-  <el-aside width="200px" class="sidebar">
+  <el-aside width="248px" class="sidebar">
     <el-menu
       default-active="2"
       class="el-menu-vertical-demo"
       @open="handleOpen"
       @close="handleClose">
+      <div class="brand">
+        <el-image
+          class="brand-icon"
+          src="https://cdn-icons-png.flaticon.com/512/4712/4712039.png"
+          alt="logo" />
+        <div class="info-card">
+          <h1>Mental Health Assistant</h1>
+          <p>Admin panel</p>
+        </div>
+      </div>
       <el-menu-item
         v-for="route in router.options.routes[0].children"
         :index="route.path"
@@ -31,3 +41,29 @@ const handleClose = (key: string, keyPath: string[]) => {
   console.log(key, keyPath);
 };
 </script>
+<style scoped lang="scss">
+.sidebar {
+  background-color: #2c3e50;
+  .brand {
+    display: flex;
+    align-items: center;
+    padding: 8px;
+    .brand-icon {
+      width: 50px;
+      height: 40px;
+      margin-right: 8px;
+    }
+    .info-card {
+      h1 {
+        font-size: 24px;
+        margin: 0;
+      }
+      p {
+        color: #bdc3c7;
+        font-size: 14px;
+        margin: 0;
+      }
+    }
+  }
+}
+</style>
