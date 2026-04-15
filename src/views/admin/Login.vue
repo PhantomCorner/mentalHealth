4cec085b5231be47704f10ae3947ba9cdb74fd05
PhantomCorner <jiaojiaobigbang@gmail.com>
2026-04-15 17:11:54 +0800

    login register form update

--- src/views/admin/Login.vue
+++ src/views/admin/Login.vue
@@ -1,3 +1,62 @@
 <template>
-  <div class="login-container">login</div>
+  <div class="login-container">
+    <el-button link @click="$router.push('/auth/register')">
+      <el-icon><Back /></el-icon>
+      Go to Register
+    </el-button>
+    <el-form
+      ref="ruleFormRef"
+      :model="formData"
+      :rules="formRules"
+      label-position="top">
+      <h1>Admin Login</h1>
+      <p>Please enter your credentials to log in.</p>
+      <el-form-item label="Username" prop="username">
+        <el-input
+          v-model="formData.username"
+          placeholder="Please input username"></el-input>
+      </el-form-item>
+      <el-form-item label="Password" prop="password">
+        <el-input
+          v-model="formData.password"
+          type="password"
+          placeholder="Please input password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="handleLogin(ruleFormRef)">
+          Login
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import type {FormInstance, FormRules} from "element-plus";
+
+import {ref} from "vue";
+const ruleFormRef = ref<FormInstance>();
+
+const formData = ref({
+  username: "",
+  password: "",
+});
+const formRules: FormRules = {
+  username: [
+    {required: true, message: "Please input username", trigger: "blur"},
+  ],
+  password: [
+    {required: true, message: "Please input password", trigger: "blur"},
+  ],
+};
+const handleLogin = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  await formEl.validate((valid: boolean, fields) => {
+    if (valid) {
+      console.log("Login with", formData.value);
+    } else {
+      console.log("Validation failed", fields);
+    }
+  });
+};
+</script>
--- src/views/admin/Register.vue
+++ src/views/admin/Register.vue
@@ -1,1 +1,61 @@
-<template>register</template>
+<template>
+  <div class="register-container">
+    <el-button link @click="$router.push('/auth/login')">
+      <el-icon><Back /></el-icon>
+      Go to Login
+    </el-button>
+    <el-form
+      ref="ruleFormRef"
+      :model="formData"
+      :rules="formRules"
+      label-position="top">
+      <h1>Admin Register</h1>
+      <p>Please fill in the form to create an account.</p>
+      <el-form-item label="Username" prop="username">
+        <el-input
+          v-model="formData.username"
+          placeholder="Please input username"></el-input>
+      </el-form-item>
+      <el-form-item label="Password" prop="password">
+        <el-input
+          v-model="formData.password"
+          type="password"
+          show-password
+          placeholder="Please input password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="handleRegister(ruleFormRef)">
+          Register
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script lang="ts" setup>
+import {ref} from "vue";
+import type {FormInstance, FormRules} from "element-plus";
+
+const ruleFormRef = ref<FormInstance>();
+const formData = ref({
+  username: "",
+  password: "",
+});
+const formRules: FormRules = {
+  username: [
+    {required: true, message: "Please input username", trigger: "blur"},
+  ],
+  password: [
+    {required: true, message: "Please input password", trigger: "blur"},
+  ],
+};
+const handleRegister = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  await formEl.validate((valid: boolean, fields) => {
+    if (valid) {
+      console.log("Registering with", formData.value);
+    } else {
+      console.log("Validation failed", fields);
+    }
+  });
+};
+</script>
